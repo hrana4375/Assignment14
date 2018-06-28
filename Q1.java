92ca13adb4f9aad71ef4dd4c105b24d2af60ecb0
hrana4375 <hrana4375@gmail.com>
2018-06-28 23:25:03 +0530

    Final Submit

---
+++ Q1.java
@@ -0,0 +1,18 @@
+package com.example.himsrana.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import static android.widget.Toast.LENGTH_SHORT;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       }
+}
+
